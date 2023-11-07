6947814a66aa72627f6c896011cc357f9fa5ad69
javie <javierromeroup@gmail.com>
2023-11-07 08:44:18 -0600

    initializing project, dependencies needed and adding docker

---
+++ src/test/java/com/javi/kjtpfinalproject/KjtpFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javi.kjtpfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjtpFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
